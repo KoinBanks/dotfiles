32d5c506e812ddb20fca534926c054c2dcb9038f
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2026-01-28 23:40:45 +0100

    Move to own file

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -102,16 +102,4 @@ return {
 			})
 		end,
 	},
-
-	{
-		"folke/lazydev.nvim",
-		ft = "lua",
-		opts = {
-			library = {
-				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-				{ path = "snacks.nvim", words = { "Snacks" } },
-				{ path = "lazy.nvim", words = { "LazyVim" } },
-			},
-		},
-	},
 }
