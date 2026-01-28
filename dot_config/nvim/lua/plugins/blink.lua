d5232c58c5978d6c97d32df28894f3d74133b4c1
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2026-01-28 23:39:04 +0100

    Add LazyDev as blink provider

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -42,13 +42,19 @@ return {
 			},
 		},
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
 			per_filetype = {
 				javascript = { "lsp", "path", "snippets" },
 				codecompanion = { "codecompanion" },
 				["grug-far"] = {},
 			},
 			providers = {
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					-- make lazydev completions top priority (see `:h blink.cmp`)
+					score_offset = 100,
+				},
 				snippets = {
 					opts = {
 						friendly_snippets = false,
