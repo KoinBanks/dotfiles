5e470c9b6a7e04a2fe19afe59e3b2fba647794ba
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2026-02-25 11:06:39 +0100

    Add ts-expand-hover

---
+++ dot_config/nvim/lua/plugins/ts-expand-hover.lua
@@ -0,0 +1,11 @@
+return {
+	"nemanjamalesija/ts-expand-hover.nvim",
+	ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+	opts = {
+		keymaps = {
+			hover = "<leader>th",
+			expand = "]t",
+			collapse = "[t",
+		},
+	},
+}
