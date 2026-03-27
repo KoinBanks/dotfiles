8502a40a62a820e36996424227f0beb903709d1f
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2026-03-27 23:52:40 +0100

    Use custom color scheme

--- winhome/dot_wezterm.lua
+++ winhome/dot_wezterm.lua
@@ -14,12 +14,38 @@ config.window_close_confirmation = "NeverPrompt"
 -- config.window_decorations = "NONE"
 config.window_background_opacity = 0.98
 config.hide_tab_bar_if_only_one_tab = true
-config.color_scheme = "Gruvbox dark, hard (base16)"
+-- config.color_scheme = "Gruvbox dark, hard (base16)"
 config.window_padding = {
 	left = 0,
 	right = 0,
 	top = 0,
 	bottom = 0,
+}
+
+config.colors = {
+	foreground = "#f9e1a4",
+	background = "#282828",
+	cursor_bg = "#f9e1a4",
+	ansi = {
+		"#282828",
+		"#e90900",
+		"#b2b100",
+		"#f8a400",
+		"#349499",
+		"#c54e84",
+		"#5baa5e",
+		"#b19b7b",
+	},
+	brights = {
+		"#99836d",
+		"#ff4630",
+		"#dce001",
+		"#ffbf2a",
+		"#7aae9a",
+		"#e67392",
+		"#86d16b",
+		"#f9e1a4",
+	},
 }
 
 wezterm.on("gui-startup", function(cmd)
