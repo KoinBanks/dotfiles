baa7f55212634354315443dae9e6ab340954c22d
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2026-04-07 15:03:42 +0200

    Manually map <M-l> so it does not print <80> like the native mapping

--- dot_config/nvim/lua/plugins/copilot.lua
+++ dot_config/nvim/lua/plugins/copilot.lua
@@ -2,5 +2,22 @@ return {
 	"zbirenbaum/copilot.lua",
 	cmd = "Copilot",
 	event = "InsertEnter",
-	opts = { suggestion = { enabled = true, auto_trigger = true } },
+	keys = {
+		{
+			"<M-l>",
+			function()
+				require("copilot.suggestion").accept()
+			end,
+			mode = "i",
+			desc = "Accept Copilot suggestion",
+		},
+	},
+	opts = {
+		suggestion = {
+			enabled = true,
+			auto_trigger = true,
+			trigger_on_accept = true,
+			keymap = { accept = false },
+		},
+	},
 }
