01989cad4ae3fece6b0ecf0385fd91443ad15f09
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2026-04-14 23:38:16 +0200

    Keep ember just in case

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -38,4 +38,15 @@ return {
 			vim.cmd.colorscheme("gruvbox")
 		end,
 	},
+	-- {
+	-- 	"ember-theme/nvim",
+	-- 	name = "ember",
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		require("ember").setup({
+	-- 			variant = "ember-soft", -- "ember" | "ember-soft" | "ember-light"
+	-- 		})
+	-- 		vim.cmd("colorscheme ember")
+	-- 	end,
+	-- },
 }
