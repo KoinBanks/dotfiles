b41609a1cb5e12cb3a5eca975da76217dba12a46
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2026-04-25 21:55:19 +0200

    Use this way of getting icons

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -1,3 +1,21 @@
+local function get_mini_icon(ctx)
+	if ctx.source_name == "Path" then
+		local is_unknown_type = vim.tbl_contains(
+			{ "link", "socket", "fifo", "char", "block", "unknown" },
+			ctx.item.data.type
+		)
+		local mini_icon, mini_hl, _ = require("mini.icons").get(
+			is_unknown_type and "os" or ctx.item.data.type,
+			is_unknown_type and "" or ctx.label
+		)
+		if mini_icon then
+			return mini_icon, mini_hl
+		end
+	end
+	local mini_icon, mini_hl, _ = require("mini.icons").get("lsp", ctx.kind)
+	return mini_icon, mini_hl
+end
+
 return {
 	"saghen/blink.cmp",
 	dependencies = { "rafamadriz/friendly-snippets" },
@@ -28,7 +46,7 @@ return {
 		completion = {
 			documentation = { auto_show = true, auto_show_delay_ms = 200 },
 			ghost_text = { enabled = false },
-			accept = { auto_brackets = { enabled = false } },
+			accept = { auto_brackets = { enabled = true } },
 			list = {
 				selection = { preselect = true },
 			},
@@ -38,18 +56,19 @@ return {
 					components = {
 						kind_icon = {
 							text = function(ctx)
-								local kind_icon, _, _ =
-									require("mini.icons").get("lsp", ctx.kind)
+								local kind_icon, kind_hl = get_mini_icon(ctx)
 								return kind_icon
 							end,
+							-- (optional) use highlights from mini.icons
 							highlight = function(ctx)
-								local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+								local _, hl = get_mini_icon(ctx)
 								return hl
 							end,
 						},
 						kind = {
+							-- (optional) use highlights from mini.icons
 							highlight = function(ctx)
-								local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+								local _, hl = get_mini_icon(ctx)
 								return hl
 							end,
 						},
