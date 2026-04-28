77dc606829d9d46f684512e3a20540084e9fa9fd
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2026-04-28 15:52:40 +0200

    Update CC config

--- dot_config/nvim/lua/plugins/codecompanion.lua
+++ dot_config/nvim/lua/plugins/codecompanion.lua
@@ -86,34 +86,35 @@ return {
 							{
 								env = {
 									url = "https://openrouter.ai/api",
-									api_key = "OPENROUTER_API_KEY",
+									api_key = vim.env.OPENROUTER_API_KEY,
 									chat_url = "/v1/chat/completions",
-								},
-								handlers = {
-									parse_message_meta = function(_, data)
-										local extra = data.extra
-										if extra and extra.reasoning then
-											data.output.reasoning = { content = extra.reasoning }
-											if data.output.content == "" then
-												data.output.content = nil
-											end
-										end
-										return data
-									end,
 								},
 								schema = {
 									model = {
-										default = "deepseek/deepseek-v3.2-speciale",
-										choices = {
-											["deepseek/deepseek-v3.2-speciale"] = {},
-											["deepseek/deepseek-v3.2"] = {},
-											["openai/gpt-5.2"] = {},
-											["minimax/minimax-m2.1"] = {},
-											["google/gemini-3-flash-preview"] = {},
-											["z-ai/glm-4.7"] = {},
-										},
+										default = "deepseek/deepseek-v4-flash",
 									},
 								},
+								-- handlers = {
+								-- 	response = {
+								-- 		---@param self CodeCompanion.HTTPAdapter
+								-- 		--- `data` is the output of the `parse_chat` handler
+								-- 		---@param data {status: string, output: {role: string?, content: string?}, extra: table}
+								-- 		---@return {status: string, output: {role: string?, content: string?, reasoning:{content: string?}?}}
+								-- 		parse_meta = function(self, data)
+								-- 			local extra = data.extra
+								-- 			if extra.reasoning_content then
+								-- 				-- codecompanion expect the reasoning tokens in this format
+								-- 				data.output.reasoning =
+								-- 					{ content = extra.reasoning_content }
+								-- 				-- so that codecompanion doesn't mistake this as a normal response with empty string as the content
+								-- 				if data.output.content == "" then
+								-- 					data.output.content = nil
+								-- 				end
+								-- 			end
+								-- 			return data
+								-- 		end,
+								-- 	},
+								-- },
 							}
 						)
 					end,
