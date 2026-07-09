51cb78a524f78ef176de81424bac0107ce00c478
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2026-07-09 10:39:49 +0200

    Fall back to empty object

--- dot_config/nvim/lua/utils.lua
+++ dot_config/nvim/lua/utils.lua
@@ -1,6 +1,10 @@
 local M = {}
 
 M.load_json = function(path)
+	if vim.uv.fs_stat(path) == nil then
+		return {}
+	end
+
 	local text = table.concat(vim.fn.readfile(path), "\n")
 	return vim.json.decode(text)
 end
