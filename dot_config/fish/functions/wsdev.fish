e27ea53a9dd7bedad7f37d224ec714cc8463b5c7
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2026-07-22 13:50:22 +0200

    Only add --host when on dev servers

--- dot_config/fish/functions/wsdev.fish
+++ dot_config/fish/functions/wsdev.fish
@@ -1,8 +1,13 @@
 function wsdev
+    set -l build_command "bun --cwd=/home/patrik/develop/repos/mis/sw/ims/ims4/Web/src/main/webapp/build2 build.ts --include=geosuite --watch"
+    if string match -q '*10.111.*' -- $argv[1]
+        set build_command "$build_command --host $argv[1]"
+    end
+
     tmux new-session -d -s wsdev \
         "localdev --open --url $argv[1]"
     tmux split-window -t wsdev \
-        "bun --cwd=/home/patrik/develop/repos/mis/sw/ims/ims4/Web/src/main/webapp/build2 build.ts --include=geosuite --watch"
+        "$build_command"
     tmux set-window-option -t wsdev synchronize-panes on
     tmux attach-session -t wsdev
 end
