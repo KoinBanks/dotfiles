d7201ff6ef4cba1de96f159c85d73cf328e46660
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2026-08-07 18:09:35 +0200

    Update script

--- dot_config/fish/functions/updateshit.fish
+++ dot_config/fish/functions/updateshit.fish
@@ -1,7 +1,13 @@
 function updateshit
     sudo apt update
     sudo apt upgrade -y
+		
     brew update
     brew upgrade -y
+
 		bunx skills update -g
+
+		pi update
+		pi update --extensions
+		chezmoi add ~/.pi/agent/settings.json
 end
