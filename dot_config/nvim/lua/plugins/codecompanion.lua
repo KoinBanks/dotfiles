24ab0ccfd55fd9a0e8dc0b6f7c97b638434299ff
Patrik Jajcay <patrik.jajcay@microstep-mis.com>
2026-08-25 15:29:16 +0200

    Rename

---
+++ dot_config/nvim/lua/plugins/codecompanion.lua
@@ -0,0 +1,74 @@
+return {
+	"olimorris/codecompanion.nvim",
+	dir = "~/develop/repos/codecompanion.nvim",
+	dev = false,
+	version = "^19.0.0",
+	enabled = true,
+	dependencies = {
+		{ "nvim-lua/plenary.nvim", branch = "master" },
+		"nvim-treesitter/nvim-treesitter",
+	},
+	keys = {
+		{
+			"<C-a>",
+			":SendToPi ",
+			mode = { "v" },
+			desc = "Code Companion inline edit",
+		},
+		{
+			"<leader>aa",
+			":CodeCompanionChat Toggle<CR>",
+			mode = { "n" },
+			silent = true,
+			desc = "Code Companion Chat",
+		},
+		{
+			"<leader>ac",
+			":CodeCompanionActions<CR>",
+			mode = { "n" },
+			silent = true,
+			desc = "Code Companion Actions",
+		},
+	},
+	config = function()
+		require("codecompanion").setup({
+			opts = {
+				log_level = "DEBUG",
+			},
+			rules = {
+				default = {
+					description = "Default rules",
+					files = {
+						"~/.agents/skills/caveman/SKILL.md",
+						"AGENTS.md",
+						"PERSONAL.md",
+					},
+				},
+				opts = {
+					chat = {
+						autoload = "default",
+						enabled = true,
+					},
+				},
+			},
+			interactions = {
+				chat = {
+					adapter = { name = "copilot", model = "gpt-5.6-luna" },
+					opts = {
+						completion_provider = "blink",
+					},
+					tools = {
+						["read_file"] = {
+							opts = {
+								require_approval_before = false,
+							},
+						},
+					},
+				},
+				inline = {
+					adapter = { name = "copilot", model = "gpt-5.6-luna" },
+				},
+			},
+		})
+	end,
+}
